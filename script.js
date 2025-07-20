0e9224e026869bfb0aae5dbd1f7b53f8af34dc96
Harshana6 <harshanagohil@gmail.com>
2025-07-20 10:47:30 +0530

    Push cleaned Weather App - API key removed and .env protected

---
+++ script.js
@@ -0,0 +1,41 @@
+const apikey = "15c14e7e0f5100d8ef24e0ee129d45f5"; 
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+const weathericon = document.querySelector(".weather-icon");
+
+async function checkweather(city) {
+    const response = await fetch(apiurl + city + `&appid=${apikey}`);
+    var data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " KM/H";
+
+    if (data.weather[0].main.toLowerCase() === "clouds") {
+        weathericon.src = "images/clouds.png";
+    } else if (data.weather[0].main.toLowerCase() === "clear") {
+        weathericon.src = "images/clear.png";
+    } else if (data.weather[0].main.toLowerCase() === "drizzle") {
+        weathericon.src = "images/drizzle.png";
+    } else if (data.weather[0].main.toLowerCase() === "humidity") {
+        weathericon.src = "images/humidity.png";
+    } else if (data.weather[0].main.toLowerCase() === "mist") {
+        weathericon.src = "images/mist.png";
+    } else if (data.weather[0].main.toLowerCase() === "rain") {
+        weathericon.src = "images/rain.png";
+    } else if (data.weather[0].main.toLowerCase() === "snow") {
+        weathericon.src = "images/snow.png";
+    } else if (data.weather[0].main.toLowerCase() === "wind") {
+        weathericon.src = "images/wind.png";
+    } else {
+        weathericon.src = "images/search.png";
+    }
+}
+
+searchbtn.addEventListener("click", () => {
+    checkweather(searchbox.value);
+});
+
